e62e54a5e8a0472a8e1c0f0dcb2d0a2622ec031d
Mavis <angel253698@gmail.com>
2017-11-07 16:25:58 +0800

    Niner, Prime v2

--- src/com/mavis/Prime2.java
+++ src/com/mavis/Prime2.java
@@ -3,17 +3,17 @@ package com.mavis;
 public class Prime2 {
 
 	public static void main(String[] args) {
-		int n = 117; 
+		int n = 117;
 		boolean prime = true;
-		for (int i=2; i<n; i++){
-			if(n % i == 0){
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
 				prime = false;
 				break;
 			}
 		}
-		if (prime){
+		if (prime) {
 			System.out.println(n + " IS PRIME NUMBER !");
-		}else{
+		} else {
 			System.out.println(n + " IS NOT PRIME NUMBER");
 		}
 	}
--- src/com/mavis/PrimeIn1000.java
+++ src/com/mavis/PrimeIn1000.java
@@ -5,13 +5,13 @@ public class PrimeIn1000 {
 	public static void main(String[] args) {
 		boolean prime = true;
 		for (int n = 2; n <= 1000; n++) {
-			for (int i = 2; i <= (n-1); i++) {
-				if (n % i == 0) {
+			for (int i = 2; i <= (n-1); i++ ) {
+				if (n % i == 0  || n % 3 == 0) {
 					prime = false;
 				} else {
 					prime = true;
 				}
-				break;
+				
 			}
 			if (prime){
 				System.out.println(n+"\t");
