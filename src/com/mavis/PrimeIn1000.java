ae94c8f05a226c25245cfbb9988c2fd262bb5498
Mavis <angel253698@gmail.com>
2017-11-07 16:34:46 +0800

    Niner, Prime, PrimeIn1000

--- src/com/mavis/PrimeIn1000.java
+++ src/com/mavis/PrimeIn1000.java
@@ -6,7 +6,7 @@ public class PrimeIn1000 {
 		boolean prime = true;
 		for (int n = 2; n <= 1000; n++) {
 			for (int i = 2; i <= (n-1); i++ ) {
-				if (n % i == 0  || n % 3 == 0) {
+				if (n % i == 0 || n % 3 == 0) {
 					prime = false;
 				} else {
 					prime = true;
