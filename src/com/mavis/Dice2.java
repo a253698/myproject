17e15d30e8b6cadf8052b63d2ce4d2108953094b
Mavis <angel253698@gmail.com>
2017-11-28 14:39:33 +0800

    Dice

---
+++ src/com/mavis/Dice2.java
@@ -0,0 +1,18 @@
+package com.mavis;
+
+public class Dice2 {
+
+	public static void main(String[] args) {
+
+		int n1;
+		int n2;
+		int n3;
+		
+		int n[] = {4 ,5 ,6 };
+		
+		for (int i=0; i<=2; i++){
+			System.out.println(n[i]);
+		}
+	}
+
+}
