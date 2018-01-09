9278d82bdfef0ea5c6d0e18bd95a7e4ebc8a96a4
Mavis <angel253698@gmail.com>
2018-01-09 16:29:20 +0800

    Shuffle

---
+++ src/com/mavis/PokerCard/Shuffle.java
@@ -0,0 +1,16 @@
+package com.mavis.PokerCard;
+
+import java.util.Random;
+
+public class Shuffle {
+
+	public static void main(String[] args) {
+		
+		int[] n = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		
+		
+
+	}
+
+}
