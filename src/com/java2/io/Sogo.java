38cd98036d9b313f7f1948b596e4d69b9a8b28df
Mavis <angel253698@gmail.com>
2018-04-17 15:42:25 +0800

    Sale

--- src/com/java2/io/Sogo.java
+++ src/com/java2/io/Sogo.java
@@ -1,41 +1,61 @@
 package com.java2.io;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Sogo {
+	Scanner scanner = new Scanner(System.in);
+	
 
 	public Sogo() {
 
 	}
 
+	public static void main(String[] args) {
+		Sogo sogo = new Sogo();
+		sogo.start();
+	}
+
 	public void inputSales() {
 		try {
-			FileOutputStream out = new FileOutputStream("SalesData.txt");
+			OutputStream fos = new FileOutputStream("SalesData.txt");
+			PrintStream out = new PrintStream(fos);
+			System.out.print("請輸入會員等級:");
+			int level = scanner.nextInt();
+			System.out.print("請輸入消費金額:");
+			int price = scanner.nextInt();
+			out.flush();
+			out.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void start(){
-		showFunctions();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("請輸入功能1~3");
-		int function = scanner.nextInt();
-		while(function !=  3){
+		int function = 0;
+		while(function != 3){
+			showFunctions();
+			function = scanner.nextInt();
 			switch(function){
-		case 3;
+			case 1:
+			inputSales();
+			break;
+			
+			case 2:
+			break;
+			
+			case 3:
 		return;
 		}
 		}
 	}
 
-	public static void main(String[] args) {
-
-	}
-
 	public void showFunctions() {
+		System.out.println("請輸入功能1~3");
 		System.out.println("1)輸入銷售紀錄");
 		System.out.println("2)印書銷售報表");
 		System.out.println("3結束程式");
