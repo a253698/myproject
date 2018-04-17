5e194a8cbb6b2e9748c226ee55a2e150b127ce53
Mavis <angel253698@gmail.com>
2018-04-17 15:10:15 +0800

    Sale

--- src/com/java2/io/Sogo.java
+++ src/com/java2/io/Sogo.java
@@ -1,19 +1,41 @@
 package com.java2.io;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
 public class Sogo {
 
-	public static void main(String[] args) {
+	public Sogo() {
+
+	}
+
+	public void inputSales() {
+		try {
+			FileOutputStream out = new FileOutputStream("SalesData.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void start(){
+		showFunctions();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("請輸入功能1~3");
-		int function = scanner.nextlnt();
-		switch(function){
-	
+		int function = scanner.nextInt();
+		while(function !=  3){
+			switch(function){
 		case 3;
 		return;
 		}
+		}
 	}
-	
-	public void showFunctions(){
+
+	public static void main(String[] args) {
+
+	}
+
+	public void showFunctions() {
 		System.out.println("1)輸入銷售紀錄");
 		System.out.println("2)印書銷售報表");
 		System.out.println("3結束程式");
