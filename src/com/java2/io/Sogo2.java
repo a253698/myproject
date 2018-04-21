31a40d1d704b332b365bb91d8e7bac2e130f1a72
Mavis <angel253698@gmail.com>
2018-04-21 22:28:26 +0800

    Sogo2

--- src/com/java2/io/Sogo2.java
+++ src/com/java2/io/Sogo2.java
@@ -1,10 +1,19 @@
 package com.java2.io;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import com.mavis.shopping.Customer;
+import com.mavis.shopping.GoldenCustomer;
+import com.mavis.shopping.SilverCustomer;
 
 public class Sogo2 {
 	Scanner scanner = new Scanner(System.in);
@@ -18,7 +27,7 @@ public class Sogo2 {
 		sogo.start();
 	}
 
-	public void inputSales() {
+	public void inputSales2() {
 		try {
 			OutputStream fos = new FileOutputStream("Sogo2SalesData.txt");
 			PrintStream out = new PrintStream(fos);
@@ -45,12 +54,74 @@ public class Sogo2 {
 				break;
 
 			case 2:
+				List<Sales> list = new ArrayList<>();
+				try {
+				FileInputStream fis = FileInputStream("Sales2.txt")
+				InputStreamReader isr = new InputStreamReader(fis);
+				// FileInputStream 和 BufferedReader 的連接橋梁
+				BufferedReader in = new BufferedReader(isr);
+				String line = in.readLine();
+				while (line != null) {
+					String[] token = line.split("\t");
+					try {
+						int type = Integer.parseInt(token[0]);
+						int amount = Integer.parseInt(token[1]);
+						Sales sales = new Sales(type, amount);
+						list.add(sales);
+					} catch (NumberFormatException e) {
+						System.out.println("資料的格式錯誤");
+						return;
+					}
+					line = in.readLine();
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// report
+			for (Sales sales: list){
+				Customer customer = null;
+				switch(sales.type){
+				case 1: 
+					customer = new Customer(sales.getAmount());
+					break;
+				case 2:
+					customer = new SilverCustomer(sales.getAmount());
+					break;
+				case 3:
+					customer = new GoldenCustomer(sales.getAmount());
+					break;
+				}
+				customer.print();
+			}
+				
 				break;
 
 			case 3:
 				return;
 			}
 		}
+	}
+	
+	public void inputSales() {
+		try {
+			FileOutputStream fos = new FileOutputStream("sales.txt", true);
+			PrintStream out = new PrintStream(fos);
+			System.out.print("請輸入會員等級:");
+			int type = scanner.nextInt();
+			System.out.print("請輸入銷售金額:");
+			int amount = scanner.nextInt();
+			out.println(type + "\t" + amount);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public void showFunctions() {
