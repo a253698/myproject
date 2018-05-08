39da3d5c477e82fd17913756952f0ff8d05f194d
Mavis <angel253698@gmail.com>
2018-05-08 15:03:56 +0800

    Stepik

---
+++ src/stepik/ArraylistName.java
@@ -0,0 +1,22 @@
+package stepik;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraylistName {
+
+	public static void main(String[] args) {
+
+		String strArrsy;
+
+		List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+
+		String[] strArray = nameList.toArray(new String[0]);
+
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.println(strArray[i]);
+		}
+
+	}
+}
