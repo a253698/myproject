579c54932ad2fd746fb918ce9cd933b5bd1ea374
Mavis <angel253698@gmail.com>
2018-05-28 22:21:29 +0800

    Snow

--- src/com/java2/snowing/Snow.java
+++ src/com/java2/snowing/Snow.java
@@ -1,33 +1,51 @@
 package com.java2.snowing;
-//為甚麼要寫snow建構子，做好初始化，之後呼價snow就不用重新設定x，可以直接運用。
 
+//為甚麼要寫snow建構子，做好初始化，之後呼價snow就不用重新設定x，可以直接運用。
+import java.awt.Canvas;
 import java.util.Random;
 
 public class Snow extends Thread {
 	Random random = new Random();
 	int x;
 	int y = 800;
+	private Canvas canvas;
 
 	public Snow() {
+		this.canvas = canvas;
 		x = random.nextInt(600);
+		y = random.nextInt(1000);
 	}
-	
-//	private static char[] repeat(String string, int i) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//    public static void main(String[] args) {
-//        System.out.println(repeat("*", 5));
-//    }
 
+	// private static char[] repeat(String string, int i) {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+	//
+	// public static void main(String[] args) {
+	// System.out.println(repeat("*", 5));
+	// }
+
+	@Override
 	public void run() {
-		for (int i = 800; i > 0; i--) {
+		for (int i = 800; i > 0; i++) {
 //			System.out.println(getName() + ":(" + x + "," + y + ")");
-			y = y - 1;
+//			y = y - 1;
+			y = y + 1;
 			int movement = random.nextInt(3) - 1;
 			x = x - movement;
-			System.out.print(new String(new char[x]).replaceAll("\0", " "));
-			System.out.println("*");}
+			
+//			canvas.repaint();
+			
+			try {
+				sleep(50);
+				//慢0.05秒
+				canvas.repaint();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+//			System.out.print(new String(new char[x]).replaceAll("\0", " "));
+//			System.out.println("*");}
 	}
+}
 }
--- src/com/java2/snowing/SnowTester.java
+++ src/com/java2/snowing/SnowTester.java
@@ -2,6 +2,8 @@ package com.java2.snowing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Canvas;
+
 
 //Quiz; 設計SNOW類別代表一顆雪X,Y,Thread飄降落
 
@@ -9,7 +11,8 @@ public class SnowTester {
 
 	public static void main(String[] arges) {
 		List<Snow> snows = new ArrayList<>();
-		for (int n = 0; n < 10; n++) {
+		for (int n = 0; n < 150; n++) {
+			Canvas canvas = null;
 			Snow snow = new Snow();
 			snow.start();
 			snows.add(snow);
