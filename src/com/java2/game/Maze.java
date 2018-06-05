6e28b7fe0e1697d009db6a863ee6ef882ac2c980
Mavis <angel253698@gmail.com>
2018-06-05 15:40:57 +0800

    VendingMachine

---
+++ src/com/java2/game/Maze.java
@@ -0,0 +1,11 @@
+package com.java2.game;
+
+public class Maze {
+	
+	/*
+	Map(4*6)
+	Trap(cannot same) use List(same) use Set(no same)
+	Player site, move(up down left right)
+	How to put Maze Player together -> put player in Map?
+	*/
+}
--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -2,6 +2,7 @@ package com.java2.oo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
 
@@ -11,7 +12,18 @@ public class VendingMachine {
 	public void on(){
 		System.out.println("Please insert coins...");
 		for(int n = 0; n <drinks.size(); n++){
-			
+			Drink drink = drinks.get(n);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+		}
+		Scanner scanner = new Scanner(System.in);
+		int data = -1;
+		while(data != 0){
+			data = scanner.nextInt();
+			switch(data){
+			case 0:
+				System.out.println("End");
+				break;
+			}
 		}
 		
 	}
