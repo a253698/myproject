96825468da424b7c24d1c4b739ca383044a0b0b0
Mavis <angel253698@gmail.com>
2018-06-17 01:21:25 +0800

    Schedule List

--- src/com/java2/schedule/ScheduleMain.java
+++ src/com/java2/schedule/ScheduleMain.java
@@ -9,18 +9,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
 public class ScheduleMain {
-
-	/*
-	 * 有一文字檔儲存了課表資料(schedule.txt) 請讀取資料後，設計程式可檢查是否有空檔? 如: 請輸入星期(1-7):2
-	 * 請輸入時(0-24):11 【有空檔】 請輸入星期(1-7):4 請輸入時(0-24):14 【有課: C004 English 到 16時 】
-	 */
 
 	public static void main(String[] args) {
 		new ScheduleMain();
 	}
-  
+
 	FileReader fr;
 
 	public ScheduleMain() {
@@ -29,106 +23,53 @@ public class ScheduleMain {
 			BufferedReader in = new BufferedReader(fr);
 			String line = in.readLine();
 			String tokens01[] = line.split(",");
-			
-			Set<String> set = new HashSet<>();
+			List<String> class01 = new ArrayList<>();
 			for (int i = 0; i < tokens01.length; i++) {
 				class01.add(tokens01[i]);
-				}
+			}
 			
-//			// String schedule = tokens[0];
-//			String classorder = tokens[0];
-//			String classname = tokens[1];
-//			int day = Integer.parseInt(tokens[2]);
-//			int classtime = Integer.parseInt(tokens[3]);
-//			int hour = Integer.parseInt(tokens[4]);
-//			Schedule s = new Schedule(classorder, classname, day, classtime, hour);
 			
-//			List<String> classorder1 =  new ArrayList<>();
-//			for() {	
-//			}
-//			List<String> classname1 =  new ArrayList<>();
-//			List<Integer> day1 =  new ArrayList<>();
-//			List<Integer> classtime1 =  new ArrayList<>();
-//			List<Integer> hour1 =  new ArrayList<>();
+			line = in.readLine();
+			String tokens02[] = line.split(",");
+			List<String> class02 = new ArrayList<>();
+			for (int i = 0; i < tokens01.length; i++) {
+				class01.add(tokens01[i]);
+			}
 			
+			line = in.readLine();
+			String tokens03[] = line.split(",");
+			List<String> class03 = new ArrayList<>();
+			for (int i = 0; i < tokens01.length; i++) {
+				class01.add(tokens01[i]);
+			}
+			
+			line = in.readLine();
+			String tokens04[] = line.split(",");
+			List<String> class04 = new ArrayList<>();
+			for (int i = 0; i < tokens01.length; i++) {
+				class01.add(tokens01[i]);
+			}
+			
+
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	
-	class Schoolclasses{
-		Set<String> class01;
-		Set<String> class02;
-		Set<String> class03;
-		Set<String> class04;
-		
-		public Schoolclasses(Set<String> class01, Set<String> class02, Set<String> class03, Set<String> class04) {
+	class Schoolclasses {
+		List<String> class01;
+		List<String> class02;
+		List<String> class03;
+		List<String> class04;
+
+		public Schoolclasses(List<String> class01, List<String> class02, List<String> class03, List<String> class04) {
 			this.class01 = class01;
 			this.class02 = class02;
 			this.class03 = class03;
 			this.class04 = class04;
 		}
 	}
-	
 
-//	class Schedule {
-//		String classorder;
-//		String classname;
-//		int day;
-//		int classtime;
-//		int hour;
-//		
-//		public Schedule(String classorder, String classname, int day, int classtime, int hour) {
-//			this.classorder = classorder;
-//			this.classname = classname;
-//			this.day = day;
-//			this.classtime = classtime;
-//			this.hour = hour;
-//		}
-
-//		public String getClassorder() {
-//			return classorder;
-//		}
-//
-//		public void setClassorder(String classorder) {
-//			this.classorder = classorder;
-//		}
-//
-//		public String getClassname() {
-//			return classname;
-//		}
-//
-//		public void setClassname(String classname) {
-//			this.classname = classname;
-//		}
-//
-//		public int getDay() {
-//			return day;
-//		}
-//
-//		public void setDay(int day) {
-//			this.day = day;
-//		}
-//
-//		public int getClasstime() {
-//			return classtime;
-//		}
-//
-//		public void setClasstime(int classtime) {
-//			this.classtime = classtime;
-//		}
-//
-//		public int getHour() {
-//			return hour;
-//		}
-//
-//		public void setHour(int hour) {
-//			this.hour = hour;
-//		}
-	}
 }
