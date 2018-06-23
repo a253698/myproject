e36c2501427b4a40bdb86820fbfefb02e36e22e6
Mavis <angel253698@gmail.com>
2018-06-24 00:20:17 +0800

    bingo

--- src/com/java2/bingo/BingoMain.java
+++ src/com/java2/bingo/BingoMain.java
@@ -9,15 +9,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class BingoMain {
-	
+
 	FileReader fr;
 
 	public static void main(String[] args) {
 		new BingoMain();
-		
 	}
-	
-	public BingoMain() {		
+
+	public BingoMain() {
 		try {
 			fr = new FileReader("bingo.txt");
 			BufferedReader in = new BufferedReader(fr);
@@ -25,37 +24,32 @@ public class BingoMain {
 			String tokens[] = line.split(",");
 			int bingochart = Integer.parseInt(tokens[0]);
 			int chartrow = (int) Math.sqrt(bingochart);
-			System.out.print(bingochart + " " + chartrow);
-			
+			String line2 = in.readLine();
+			String tokens2[] = line2.split(",");
+			List<Integer> ans = new ArrayList<>();
+			for (int i = 1; i <= tokens2.length; i++) {
+				int j = Integer.parseInt(tokens2[i]);
+				ans.add(j);
+			}
+			List<Integer> bingo = new ArrayList<>();
+			for (int n = 1; n <= bingochart; n++) {
+				bingo.add(n);
+			}
+			Collections.shuffle(bingo);
+
+			for (int m = 1; m <= bingochart; m++) {
+				System.out.print(bingo.get(m) + "\t");
+				if (m % 5 == 0) {
+					System.out.println();
+				}
+
+			}
+
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public void PrintCard() {
-	List<Integer> bingo = new ArrayList<>();
-		
-		for(int n = 1; n <= 25 ; n++) {
-			bingo.add(n);
-		}
-//		System.out.print(bingo);
-		Collections.shuffle(bingo);
-//		System.out.print(bingo);
-		
-		for(int m = 1; m <= 25 ; m++ ) {
-			System.out.print(bingo.get(m) + "\t");
-			if( m % 5 == 0) {
-				System.out.println();
-			}
-			
-		}
-			
-	}
-	
 
 }
