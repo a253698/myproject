8eb592abce45b440288d19e53eeafc8c8fa17456
Mavis <angel253698@gmail.com>
2018-06-24 00:41:00 +0800

    bingo

--- src/com/java2/bingo/BingoChart.java
+++ src/com/java2/bingo/BingoChart.java
@@ -1,14 +1,44 @@
 package com.java2.bingo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BingoChart {
 
 	int bingochart;
 	int chartrow;
-	
-	public BingoChart(int bingochart, int chartrow) {
+	List<Integer> ans;
+
+	public BingoChart(int bingochart, int chartrow, List<Integer> ans) {
 		this.bingochart = bingochart;
 		this.chartrow = chartrow;
-	
+		this.ans = ans;
+	}
+
+	public void PrintChart() {
+
+		List<Integer> bingo = new ArrayList<>();
+		for (int n = 1; n <= bingochart; n++) {
+			bingo.add(n);
+		}
+		Collections.shuffle(bingo);
+
+		System.out.println(bingo);
+
+		for (int m = 1; m <= bingochart; m++) {
+			for (int k = 1; k <= 10; k++) {
+				if (bingo.get(m) == ans.get(k)) {
+					bingo.set(k, 0);
+				}
+			}
+			System.out.print(bingo.get(m) + "\t");
+			if (m % chartrow == 0) {
+				System.out.println();
+			}
+
+		}
+
 	}
 
 	public int getBingochart() {
@@ -25,6 +55,14 @@ public class BingoChart {
 
 	public void setChartrow(int chartrow) {
 		this.chartrow = chartrow;
+	}
+
+	public List<Integer> getAns() {
+		return ans;
+	}
+
+	public void setAns(List<Integer> ans) {
+		this.ans = ans;
 	}
 
 }
--- src/com/java2/bingo/BingoMain.java
+++ src/com/java2/bingo/BingoMain.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class BingoMain {
 
 	FileReader fr;
-	Integer X;
+
 
 	public static void main(String[] args) {
 		new BingoMain();
@@ -27,72 +27,19 @@ public class BingoMain {
 			line = in.readLine();
 			String tokens[] = line.split(",");
 			List<Integer> ans = new ArrayList<>();
-			for (int i = 1; i <= tokens.length; i++) {
-				int j = Integer.parseInt(tokens[i]);
-				ans.add(j);
-			}
-			List<Integer> bingo = new ArrayList<>();
-			for (int n = 1; n <= bingochart; n++) {
-				bingo.add(n);
-			}
-			Collections.shuffle(bingo);
+//			for (int i = 1; i <= tokens.length; i++) {
+//				int j = Integer.parseInt(tokens[i]);
+//				ans.add(j);
+//			}
+			BingoChart bc = new BingoChart(bingochart, chartrow, ans);
+			bc.PrintChart();
 			
-			for (int m = 1; m <= bingochart; m++) {
-				for (int k = 1; k <= tokens.length; k++) {
-					if (bingo.get(m) == ans.get(k)) {
-						bingo.set(k, X);
-					}
-				}
-				System.out.print(bingo.get(m) + "\t");
-				if (m % chartrow == 0) {
-					System.out.println();
-				}
-
-			}
-
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		class Chart {
-			int bingochart;
-			int chartrow;
-			List<Integer> ans;
-			
-			public Chart(int bingochart, int chartrow, List<Integer> ans) {
-				this.bingochart = bingochart;
-				this.chartrow = chartrow;
-				this.ans = ans;
-			}
-			
-			public int getBingochart() {
-				return bingochart;
-			}
-
-			public void setBingochart(int bingochart) {
-				this.bingochart = bingochart;
-			}
-
-			public int getChartrow() {
-				return chartrow;
-			}
-
-			public void setChartrow(int chartrow) {
-				this.chartrow = chartrow;
-			}
-
-			public List<Integer> getAns() {
-				return ans;
-			}
-
-			public void setAns(List<Integer> ans) {
-				this.ans = ans;
-			}
-
-			
-		}
 	}
 
 }
