935e58e20632037be50246b2435ad6489b4b7e31
Mavis <angel253698@gmail.com>
2018-06-25 19:13:14 +0800

    ooSchedule getter setter

--- src/com/java2/ooSchedule/Classes.java
+++ src/com/java2/ooSchedule/Classes.java
@@ -2,9 +2,51 @@ package com.java2.ooSchedule;
 
 public class Classes {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		String ClassName;
+		String ClassNo;
+		int day;
+		int time;
+		int starttime;
+		
+		public Classes(String ClassName, String ClassNo,int day,int time,int starttime) {
+			this.ClassName = ClassName;
+			this.ClassNo = ClassNo;
+			this.day = day;
+			this.time = time;
+			this.starttime = starttime;
+		}
+		
+		public String getClassName() {
+			return ClassName;
+		}
+		public void setClassName(String className) {
+			ClassName = className;
+		}
+		public String getClassNo() {
+			return ClassNo;
+		}
+		public void setClassNo(String classNo) {
+			ClassNo = classNo;
+		}
+		public int getDay() {
+			return day;
+		}
+		public void setDay(int day) {
+			this.day = day;
+		}
+		public int getTime() {
+			return time;
+		}
+		public void setTime(int time) {
+			this.time = time;
+		}
+		public int getStarttime() {
+			return starttime;
+		}
+		public void setStarttime(int starttime) {
+			this.starttime = starttime;
+		}
+		
+		
+	
 }
