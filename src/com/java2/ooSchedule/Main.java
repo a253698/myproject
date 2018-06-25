f0a32424538685ec2708ade837230e8a34f902a4
Mavis <angel253698@gmail.com>
2018-06-25 19:26:07 +0800

    ooSchedule

--- src/com/java2/ooSchedule/Main.java
+++ src/com/java2/ooSchedule/Main.java
@@ -1,10 +1,22 @@
 package com.java2.ooSchedule;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+	
+	FileReader fr;
+	List<Classes> cl = new ArrayList<>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	}
+	
+	public Main() {
+		fr = new FileReader("schedule.txt");
+		BufferedReader in = new BufferedReader(fr);
 	}
 
 }
